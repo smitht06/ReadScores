6bb7d99460d8d08660f428fadc8a419735c3b1f1
Anthony Smith <as361@students.uwf.edu>
2020-03-11 20:46:28 -0400

    beging writing file read method

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,6 +1,18 @@
+import java.io.*;
+import java.util.*;
 
 public class ReadFile {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        String [] master = new String[200];
+
+    }
+
+    private static void readFile() throws FileNotFoundException {
+        try {
+            BufferedReader read = null;
+            read = new BufferedReader(new FileReader("scores.txt"));
+        }catch(FileNotFoundException e){
+            System.out.println("Check file name, file not found");
+        }
     }
 }
