e90510371e3d1c1204537632c5476d0375888021
Anthony Smith <as361@students.uwf.edu>
2020-03-13 15:31:02 -0400

    2d array is now array of doubles

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -4,20 +4,19 @@ import java.util.Arrays;
 public class ReadFile {
     public static void main(String[] args){
         String [] master = new String[125];
-        String [][] twoDMaster = new String[5][23];
+        double [][] twoDMaster = new double[5][23];
         master = readFile();
-        for(int i =0; i < master.length; i++){
-            System.out.println(master[i]);
-        }
         int h = 4;
         for(int i = 0; i < twoDMaster.length; i++){
             for (int j = 0; j < twoDMaster[0].length; j++){
                 h++;
-                twoDMaster[i][j]=master[h];
+                twoDMaster[i][j]=Double.parseDouble(master[h]);
 
             }
         }
-        System.out.println(Arrays.toString(twoDMaster[0]));
+        for(int i = 0; i < twoDMaster.length; i++){
+            System.out.println(Arrays.toString(twoDMaster[i]));
+        }
 
     }
 
