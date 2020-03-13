2a8c2517e47875d1923e597cf4d338f58d19e1a6
Anthony Smith <as361@students.uwf.edu>
2020-03-13 13:00:39 -0400

    2d array loaded

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,13 +1,23 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class ReadFile {
     public static void main(String[] args){
         String [] master = new String[125];
-        //String [][] twoDMaster = new String[][];
+        String [][] twoDMaster = new String[5][23];
         master = readFile();
         for(int i =0; i < master.length; i++){
             System.out.println(master[i]);
         }
+        int h = 4;
+        for(int i = 0; i < twoDMaster.length; i++){
+            for (int j = 0; j < twoDMaster[0].length; j++){
+                h++;
+                twoDMaster[i][j]=master[h];
+
+            }
+        }
+        System.out.println(Arrays.toString(twoDMaster[0]));
 
     }
 
