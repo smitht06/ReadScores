44b2d880faa50baf0e67fc1c617c6b5b424de9bb
Anthony Smith <as361@students.uwf.edu>
2020-03-13 12:11:51 -0400

    array and file reader working

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,18 +1,36 @@
 import java.io.*;
-import java.util.*;
 
 public class ReadFile {
     public static void main(String[] args){
-        String [] master = new String[200];
+        String [] master = new String[125];
+        //String [][] twoDMaster = new String[][];
+        master = readFile();
+        for(int i =0; i < master.length; i++){
+            System.out.println(master[i]);
+        }
 
     }
 
-    private static void readFile() throws FileNotFoundException {
+    private static String[] readFile(){
+        String line = "";
+        BufferedReader file = null;
+        String [] master = new String[125];
+        int i = 0;
         try {
-            BufferedReader read = null;
-            read = new BufferedReader(new FileReader("scores.txt"));
-        }catch(FileNotFoundException e){
+
+            file = new BufferedReader(new FileReader("Scores.txt"));
+            while((line = file.readLine()) != null){
+                master[i] = line;
+                i++;
+            }
+
+
+        }catch(IOException e){
             System.out.println("Check file name, file not found");
         }
+
+
+        return master;
     }
+
 }
