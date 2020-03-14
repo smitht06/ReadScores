4ef5370e87f0cb2c42c0fba769d19a8596f8f0f4
Anthony Smith <as361@students.uwf.edu>
2020-03-14 08:52:31 -0400

    mean method working

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -4,22 +4,20 @@ import java.util.Arrays;
 public class ReadFile {
     public static void main(String[] args){
         String [] master = new String[125];
-        double [][] twoDMaster = new double[5][23];
+        double [][] twoDMaster = new double[5][24];
         master = readFile();
         int h = 4;
         for(int i = 0; i < twoDMaster.length; i++){
             for (int j = 0; j < twoDMaster[0].length; j++){
                 h++;
                 twoDMaster[i][j]=Double.parseDouble(master[h]);
-
             }
         }
         for(int i = 0; i < twoDMaster.length; i++){
             System.out.println(Arrays.toString(twoDMaster[i]));
         }
-
+        System.out.println(mean(twoDMaster[0]));
     }
-
     private static String[] readFile(){
         String line = "";
         BufferedReader file = null;
@@ -32,14 +30,19 @@ public class ReadFile {
                 master[i] = line;
                 i++;
             }
-
-
         }catch(IOException e){
             System.out.println("Check file name, file not found");
         }
-
-
         return master;
+    }
+    private static double mean(double[] scores){
+        double sum = 0;
+        double mean;
+        for(int i = 0; i < scores.length; i++){
+            sum += scores[i];
+        }
+        mean = sum/scores.length;
+        return mean;
     }
 
 }
