06c357dc95bee26e1a32f7c03edcc71aa34cb1cf
Anthony Smith <as361@students.uwf.edu>
2020-03-14 16:58:39 -0400

    significant diff method complete

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,7 +1,9 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class ReadFile {
+    private static DecimalFormat myDF = new DecimalFormat("###.###");
     public static void main(String[] args){
         String [] master = new String[125];
         double [][] twoDMaster = new double[5][24];
@@ -13,12 +15,15 @@ public class ReadFile {
                 twoDMaster[i][j]=Double.parseDouble(master[h]);
             }
         }
-        for(int i = 0; i < twoDMaster.length; i++){
-            System.out.println(Arrays.toString(twoDMaster[i]));
+        for (double[] doubles : twoDMaster) {
+            System.out.println(Arrays.toString(doubles));
         }
         System.out.println(mean(twoDMaster[0]));
-        System.out.println(standardDev(mean(twoDMaster[0]),twoDMaster[0]));
+        System.out.println(standardDev(mean(twoDMaster[1]),twoDMaster[1]));
+        System.out.println(meanDiffSignificance(twoDMaster[2],twoDMaster[3]));
     }
+
+
     private static String[] readFile(){
         String line = "";
         BufferedReader file = null;
@@ -52,6 +57,26 @@ public class ReadFile {
         }
         double stdDeviation = Math.sqrt(summation/scores.length);
         return stdDeviation;
+    }
+    private static boolean meanDiffSignificance(double[] scores1, double[] scores2){
+        double m1 = mean(scores1);
+        double m2 = mean(scores2);
+        double sumation1 = 0;
+        double sumation2 = 0;
+        double s2 = 0;
+        double t = 0;
+        for(double score : scores1){
+            sumation1 += (score - m1) * (score - m1);
+        }
+        for (double score : scores2){
+            sumation2 += (score - m2) * (score - m2);
+        }
+        s2 = (sumation1 + sumation2)/(scores1.length+scores2.length);
+        double denominator = Math.sqrt((s2/scores1.length)+(s2/scores2.length));
+        t = (m1-m2)/denominator;
+        System.out.println(myDF.format(t));
+        System.out.println(t);
+        return Math.abs(t) > 2.25;
     }
 
 }
